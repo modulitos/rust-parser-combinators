59b319f6b76b93a5dcdeadf1676cdcfc2b5e2882
modulitos <you@example.com>
2019-07-26 01:05:03 -0700

    one/zero or more combinator

--- src/lib.rs
+++ src/lib.rs
@@ -142,3 +142,65 @@ fn right_combinator() {
     assert_eq!(Err("oops"), tag_opener.parse("oops"));
     assert_eq!(Err("!oops"), tag_opener.parse("<!oops"));
 }
+
+
+// We've dealt with this already in our identifier parser, but it was
+// all done manually there. Not surprisingly, the code for the general
+// idea isn't all that different.
+fn one_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+where
+    P: Parser<'a, A>,
+{
+    move |mut input| {
+        let mut result = Vec::new();
+
+        if let Ok((next_input, first_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(first_item);
+        } else {
+            return Err(input);
+        }
+
+        while let Ok((next_input, next_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(next_item);
+        }
+
+        Ok((input, result))
+    }
+}
+
+
+fn zero_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+where
+    P: Parser<'a, A>,
+{
+    move |mut input| {
+        let mut result = Vec::new();
+
+        while let Ok((next_input, next_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(next_item);
+        }
+
+        Ok((input, result))
+    }
+}
+
+#[test]
+fn one_or_more_combinator() {
+    let parser = one_or_more(match_literal("ha"));
+    assert_eq!(Ok(("", vec![(), (), ()])), parser.parse("hahaha"));
+    assert_eq!(Err("ahah"), parser.parse("ahah"));
+    assert_eq!(Err(""), parser.parse(""));
+    assert_eq!(Err("foobar"), parser.parse("foobar"));
+}
+
+#[test]
+fn zero_or_more_combinator() {
+    let parser = zero_or_more(match_literal("ha"));
+    assert_eq!(Ok(("", vec![(), (), ()])), parser.parse("hahaha"));
+    assert_eq!(Ok(("ahah", vec![])), parser.parse("ahah"));
+    assert_eq!(Ok(("", vec![])), parser.parse(""));
+    assert_eq!(Ok(("foobar", vec![])), parser.parse("foobar"));
+}
